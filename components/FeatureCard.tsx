2459164d69617c9a6423a5980806f6c0c3ae71b9
Patryk Heljasinski <heljasinskip@gmail.com>
2021-05-06 01:51:29 +0200

    Disabled cards shadow

--- components/FeatureCard.tsx
+++ components/FeatureCard.tsx
@@ -11,8 +11,8 @@ const Wrapper = styled.div`
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
-  border-radius: 0.5rem;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  border-radius: 1rem;
+  /* box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24); */
   transition: 0.3s all;
 `;
 
